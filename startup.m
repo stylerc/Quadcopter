100bf16e29c77c0781753bcdf59251893b9db167
Dan <dan.durusky@gmail.com>
2018-03-31 11:03:42 -0400

    added physical properties

--- startup.m
+++ startup.m
@@ -1,7 +1,7 @@
-m = 550; %g, quadcopter mass
-g = 9.8; %m/s gravity
+% From feasibility report
 
-%frame is X shaped, symmetrical
+%----- Physical Parameters -----
+% frame is X shaped, symmetrical
 
 % x is forward
 % y is to the left
@@ -11,10 +11,10 @@ g = 9.8; %m/s gravity
 % pitch angle (?) rotation around the y axis.
 % yaw angle (?) rotation around the z axis.
 
-%vrot_i is rotor angular velocity, 1-4
+m = 3.65; %kg, quadcopter mass
+wb = 0.6; %m, quadcopter wheelbase
 
-%-------Motor Parameteres------
-
+%----- Motor Parameteres -----
 R = 0.0005; %electric resistance (Ohm)
 L = 0.0003 % electric inductance (H)
 b = 0.0009; % motor viscous friction constant (N.m.s)
@@ -22,12 +22,12 @@ J = 0.0008; % moment of inertia of the rotor (kg.m^2)
 K = 0.001; % electromotive force constant / motor torque constant (RPM/V)
 
 %Constants for thrust equation
-
 pitch = 7; %inches
 d = 20; %propeller diameter
 v0 = 0; %airspeed in Z
 x1 = 4.392399*10^-8;
 x2 = 4.23333*10^-4;
 
-%-----------------------------
 
+%---- Simulation Constants -----
+g = 9.8; %m/s gravity
