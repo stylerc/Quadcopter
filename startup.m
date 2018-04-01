179b5745f06ef80cd7dd902907e519bfd7096e8e
stylerc <stylerc@gmail.com>
2018-03-31 22:34:07 -0400

    abstracted effective area, density of air, coeff of drag

--- startup.m
+++ startup.m
@@ -55,6 +55,13 @@ g = 9.8; %m/s gravity
 m = 3.65; %kg, quadcopter mass
 wb = 0.6; %m, quadcopter wheelbase
 
+dair = 1.225; %density of air
+cdrag = 1.3; %coeff of drag
+aetop=.8494; %effective area of top of qc
+aeside=aetop/10; %effective area of side of qc
+aefront=aetop/10;  %effective area of front of qc
+
+
 %----- Initial Conditions -----
 xinitial=0;
 yinitial=0;
