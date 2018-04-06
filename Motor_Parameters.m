cc1074385bb0bcf90dd1e3006789c79230e48d6d
brianbojarski <brianbojarski@gmail.com>
2018-04-05 23:13:01 -0400

    faster response time!

--- Motor_Parameters.m
+++ Motor_Parameters.m
@@ -2,16 +2,16 @@
 V_bat = 44.4; %battery voltage (V) 
 R = 0.000230; %electric resistance (Ohm), .0005
 L = 0.000224; % electric inductance (H)
-b = 0.0015; % motor viscous friction constant (N.m.s)
-J = 0.00007; % moment of inertia of the rotor (kg.m^2)
-Ke = 0.001; % electromotive force constant 
-Kt = 0.001;    % motor torque constant 
+b = 0.01; % motor viscous friction constant (N.m.s)
+J = 0.0007; % moment of inertia of the rotor (kg.m^2)
+Ke = 0.0734; % electromotive force constant 
+Kt = 0.076;    % motor torque constant 
 idle = 0; % idle motors to reduce initial "slack"
 
 %Constants for thrust equation
 pitch = 7; %inches
 d = 20; %propeller diameter
-v0 = 0; %airspeed in Z
+%v0 = 0; %airspeed in Z
 x1 = 4.392399*10^-8;
 x2 = 4.23333*10^-4;
 
