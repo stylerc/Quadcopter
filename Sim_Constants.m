257f51ba3c120423dad386e481f338cdce93732f
Dan <dan.durusky@gmail.com>
2018-04-16 17:31:27 -0400

    added Izz

--- Sim_Constants.m
+++ Sim_Constants.m
@@ -3,11 +3,15 @@ g = 9.8; %m/s gravity
 m = 3.65; %kg, quadcopter mass
 wb = 0.6; %m, quadcopter wheelbase
 
+Izz = 16.25e-2;  %added 4/16 by Dan D. 
+
 dair = 1.225; %density of air
 cdrag = 1.3; %coeff of drag
 aetop=.8494; %effective area of top of qc
 aeside=aetop/10; %effective area of side of qc
-aefront=aetop/10;  %effective area of front of qc
+aefront=aetop/10;  %effective area of front of qc 
+
+
 
 
 %------------- K filter constants ------------
